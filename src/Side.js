bd3eb51fe87fb7f354c44c92cf3499b10c12a7b5
Sasha Kipnis <17sashak@gmail.com>
2019-04-16 16:22:03 -0400

    Style and import images to Side component

--- src/Side.js
+++ src/Side.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import quill from './quill.svg'
+import plusIcon from './new.png'
+import plusHover from './new-hover.png'
 
 const Side = () => {
     return (
         <div classname="Side">
-            Sidebar
+            <div classname="logo">
+                <img src={quill} alt='Scribble'/>
+            </div>
+            <a href='/notes'>
+                <img src={plusHover} alt='New Note'/> 
+                <img src={plusIcon} alt='New Note'/> 
+            </a>
+
+            <div classname="signOut">
+                <button>
+                    <i className = "fa fa-sign-out">
+                    </i>
+                </button>
+            </div>
+
         </div>
+
+
     )
 }
 
