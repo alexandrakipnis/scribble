6bbb7c23f2a1f2ccd9a8b74579cd0c5cc541dfa8
Alex Chabot <chabota@purdue.edu>
2019-04-16 21:55:39 -0400

    Notes from Array

--- src/List.js
+++ src/List.js
@@ -2,23 +2,29 @@ import React from 'react'
 import "./NoteList.css"
 
 const List = () => {
+    const notes = [1, 2, 3]
+    
     return(
         <div className="NoteList">
           <h3>Notes</h3>
           <ul id="notes">
             <a className="active">
-              <li>
-                <div className="note">
-                  <div className="note-title">
-                    Kohlrabi welsh
-                  </div>
-                  <div class="note-body">
-                    <p>
-                      Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
-                    </p>
-                  </div>
-                </div>
-              </li>
+                {
+                    notes.map(note => (
+                        <li>
+                            <div className="note">
+                            <div className="note-title">
+                                Kohlrabi welsh
+                            </div>
+                            <div class="note-body">
+                                <p>
+                                Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+                                </p>
+                            </div>
+                            </div>
+                        </li>
+                    ))
+                }
             </a>
         </ul>
     </div>
