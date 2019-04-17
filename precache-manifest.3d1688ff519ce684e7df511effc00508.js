40aa48d58caba024966b4be4ae6af3bdb3dfbe54
Sasha Kipnis <17sashak@gmail.com>
2019-04-17 11:41:42 -0400

    Updates

---
+++ precache-manifest.3d1688ff519ce684e7df511effc00508.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0025e4cd048852c7b91",
+    "url": "/scribble/static/css/main.05f9b27a.chunk.css"
+  },
+  {
+    "revision": "e0025e4cd048852c7b91",
+    "url": "/scribble/static/js/main.6f1322e7.chunk.js"
+  },
+  {
+    "revision": "9a47f608eb1b35504fe4",
+    "url": "/scribble/static/js/runtime~main.b104c98f.js"
+  },
+  {
+    "revision": "275e4379a3dfadc7e482",
+    "url": "/scribble/static/js/2.23214b18.chunk.js"
+  },
+  {
+    "revision": "d2ea47fafd7f81170964fc62760dcc06",
+    "url": "/scribble/static/media/quill.d2ea47fa.svg"
+  },
+  {
+    "revision": "2718a4149b26b17970929976ad78e75f",
+    "url": "/scribble/static/media/new.2718a414.png"
+  },
+  {
+    "revision": "751b2c31d2ca166109e010a7ae16c8c7",
+    "url": "/scribble/static/media/new-hover.751b2c31.png"
+  },
+  {
+    "revision": "6a6cdee044a6abef42da8925bae3272d",
+    "url": "/scribble/index.html"
+  }
+];
