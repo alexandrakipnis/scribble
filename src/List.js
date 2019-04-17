4e85b8fd30c191415f207287844cf5fb104c0e2a
Alex Chabot <chabota@purdue.edu>
2019-04-16 22:13:56 -0400

    Notes more dynamic

--- src/List.js
+++ src/List.js
@@ -3,17 +3,33 @@ import "./NoteList.css"
 import Note from './Note'
 
 const List = () => {
-    const notes = [1, 2, 3]
+    const notes = [
+        {
+            id: 1,
+            title: 'Why I <3 JS',
+            body: 'Because I like code, and JS is code.',
+        },
+        {
+            id: 2,
+            title: 'Thoughts on breakfast',
+            body: 'I love it!',
+        },
+        {
+            id: 3,
+            title: 'Watching the first episode of Black Mirror with your parents',
+            body: 'Don\'t.',
+        },
+    ]
     
     return(
         <div className="NoteList">
           <h3>Notes</h3>
           <ul id="notes">
             <a className="active">
-                { notes.map(note => <Note />)}
+                { notes.map(note => <Note note={note} />)}
             </a>
-        </ul>
-    </div>
+            </ul>
+        </div>
     )
 }
 
