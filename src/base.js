2b7da7ea4f0ff7c62285b2305bf6aee9d095f31e
Sasha Kipnis <17sashak@gmail.com>
2019-04-17 13:18:43 -0400

    Add firebase

---
+++ src/base.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import database from 'firebase/database'
+import Rebase from 're-base'
+//Initialize Firebase
+const config = {
+    apiKey: "AIzaSyAxzFARpr5XpEuc1BeqiPeCcrxwZ10TyLE",
+    authDomain: "scribble-cs252.firebaseapp.com",
+    databaseURL: "https://scribble-cs252.firebaseio.com",
+    projectId: "scribble-cs252",
+    storageBucket: "scribble-cs252.appspot.com",
+    messagingSenderId: "1072152629926"
+  };
+  const app = firebase.initializeApp(config)
+  const db = database(app)
+  
+  export default Rebase.createClass(db)
+
