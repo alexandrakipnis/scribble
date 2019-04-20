d3729ab023cea7048a226f04cf765ddf28741788
Sasha Kipnis <17sashak@gmail.com>
2019-04-19 20:24:50 -0400

    Major firebase fix

---
+++ build/precache-manifest.bb040d02c4e8ebe1cff72e8d9d4cd7bb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0acf69e2d3a83af8242f",
+    "url": "/static/css/main.60de0d1a.chunk.css"
+  },
+  {
+    "revision": "0acf69e2d3a83af8242f",
+    "url": "/static/js/main.048363fc.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "12d44034e1f5eeab5dd4",
+    "url": "/static/js/2.94a68298.chunk.js"
+  },
+  {
+    "revision": "d2ea47fafd7f81170964fc62760dcc06",
+    "url": "/static/media/quill.d2ea47fa.svg"
+  },
+  {
+    "revision": "2718a4149b26b17970929976ad78e75f",
+    "url": "/static/media/new.2718a414.png"
+  },
+  {
+    "revision": "751b2c31d2ca166109e010a7ae16c8c7",
+    "url": "/static/media/new-hover.751b2c31.png"
+  },
+  {
+    "revision": "ffe5e6be7ca1727b5bd657a360bcfc2c",
+    "url": "/static/media/google.ffe5e6be.svg"
+  },
+  {
+    "revision": "ec738428191b44dd3d200631285ad9a4",
+    "url": "/static/media/backgroundimage.ec738428.jpg"
+  },
+  {
+    "revision": "2a5235f84f3576ee91c1335eeda2b390",
+    "url": "/index.html"
+  }
+];
